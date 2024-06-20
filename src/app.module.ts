d72a3e286518a4f84488f2921a0683ec4d7e7c1a
adenita <nittaluta@gmail.com>
2024-06-20 19:38:52 +0200

    added typeorm module config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ConfigModule} from "@nestjs/config";
+import {DatabaseModule} from "./database.module";
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot(),
+      DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
