3bf5d5877933d7338c9a8061f13572208f6bcd52
adenita <nittaluta@gmail.com>
2024-06-22 21:02:10 +0200

    added loan entity

---
+++ src/modules/loan/loan.entity.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from '../user/user.entity';
+import { Book } from '../book/book.entity';
+
+@Entity()
+export class Loan {
+  @PrimaryGeneratedColumn()
+  loan_id: number;
+
+  @ManyToOne(() => User, (user) => user.loans)
+  user: User;
+
+  @ManyToOne(() => Book, (book) => book.loans)
+  book: Book;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  loan_date: Date;
+
+  @Column({ type: 'timestamp', nullable: true })
+  return_date: Date;
+}
