54b4f051645b81fc67ae34bea9a21b5a2db7bc79
adenita <nittaluta@gmail.com>
2024-06-22 19:28:44 +0200

    updated path to match

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {ConfigModule} from "@nestjs/config";
-import {DatabaseModule} from "./database.module";
+import {DatabaseModule} from "./database/database.module";
 
 @Module({
   imports: [
