ba85c0a3a408b99f4ceb67b8c6eae331fb9ff2cb
adenita <nittaluta@gmail.com>
2024-06-22 20:39:14 +0200

    updated migration

--- src/database/migrations/1719081343377-CreateBookTable.ts
+++ src/database/migrations/1719081343377-CreateBookTable.ts
@@ -4,13 +4,11 @@ export class CreateBookTable1719081343377 implements MigrationInterface {
     name = 'CreateBookTable1719081343377'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "author" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, CONSTRAINT "PK_5a0e79799d372fe56f2f3fa6871" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "book" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, CONSTRAINT "PK_a3afef72ec8f80e6e5c310b28a4" PRIMARY KEY ("id"))`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`DROP TABLE "book"`);
-        await queryRunner.query(`DROP TABLE "author"`);
     }
 
 }
