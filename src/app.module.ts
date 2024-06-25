df84f1ab7aa825a6dad1dfddeb7323fad2fbd9c3
adenita <nittaluta@gmail.com>
2024-06-25 19:29:29 +0200

    refactor app module to include auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
-import { PassportModule } from '@nestjs/passport';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    DatabaseModule,
-    PassportModule.register({ defaultStrategy: 'jwt' }),
-  ],
+  imports: [ConfigModule.forRoot(), DatabaseModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
