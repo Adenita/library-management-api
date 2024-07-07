d920e02cfa0dc90ce45705cd2237684cfe381ee6
adenita <nittaluta@gmail.com>
2024-07-07 19:39:40 +0200

    refactor list transport

--- src/modules/user/mapper/user.mapper.ts
+++ src/modules/user/mapper/user.mapper.ts
@@ -1,12 +1,18 @@
 import { User } from '../entity/user.entity';
 import { Mapper } from '../../../shared/Mapper';
+import { UserShortDto } from '../dto/user-short.dto';
+import { UserListDto } from '../dto/user-list.dto';
 
 export class UserMapper {
-  static toListTransport(users: User[]) {
-    return users.map((user: User) => this.toShortDto(user));
+  static toListTransport(users: User[]): UserListDto {
+    return { items: this.toListDto(users) } as UserListDto;
   }
 
-  static toEntity(dto: any) {
+  private static toListDto(users: User[]): UserShortDto[] {
+    return users.map((user: User) => Mapper.toDto(UserShortDto, user));
+  }
+
+  static toEntity(dto: any): User {
     return Mapper.toEntity(User, dto);
   }
 }
