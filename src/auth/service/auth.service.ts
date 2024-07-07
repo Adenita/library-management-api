2eab828a4e8108029e36abc6a338c8361682708d
adenita <nittaluta@gmail.com>
2024-07-07 21:47:14 +0200

    add register and login methods

--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -1,6 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../../modules/user/service/user.service';
-import { TokenService } from './token.service';
+import { Key, TokenService } from './token.service';
+import { UserCreateDto } from '../../modules/user/dto/user-create.dto';
+import { User } from '../../modules/user/entity/user.entity';
+import { GeneralMapper } from '../../shared/general.mapper';
+import { UserLoginDto } from '../dto/user-login.dto';
+import { TokenDto } from '../dto/token.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -8,4 +14,43 @@ export class AuthService {
     private readonly userService: UserService,
     private readonly tokenService: TokenService,
   ) {}
+
+  async register(userCreateDto: UserCreateDto): Promise<User> {
+    const hashedPassword = await bcrypt.hash(userCreateDto.password, 10);
+    const newUserDto: UserCreateDto = {
+      ...userCreateDto,
+      password: hashedPassword,
+    };
+
+    return await this.userService.createOrThrow(
+      GeneralMapper.toEntity(User, newUserDto),
+    );
+  }
+
+  async login(userLoginDto: UserLoginDto, accessKey: Key, refreshKey: Key) {
+    const user: User = await this.userService.findByIdOrThrow(
+      userLoginDto.username,
+    );
+
+    if (
+      !user ||
+      !(await bcrypt.compare(userLoginDto.password, user.password))
+    ) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const accessToken: string = await this.tokenService.generateAccessToken(
+      { username: user.username },
+      accessKey,
+    );
+    const refreshToken: string = await this.tokenService.generateRefreshToken(
+      user,
+      refreshKey,
+    );
+
+    return {
+      accessToken,
+      refreshToken,
+    } as TokenDto;
+  }
 }
