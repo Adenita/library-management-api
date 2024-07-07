73e903392112e507fb0169ecc7b947dfc3394746
adenita <nittaluta@gmail.com>
2024-07-07 21:32:38 +0200

    move token management to token service

--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -1,111 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { RefreshTokenRepository } from '../../modules/user/repository/refresh-token.repository';
-import { User } from '../../modules/user/entity/user.entity';
-import { RefreshToken } from '../../modules/user/entity/refresh-token.entity';
 import { UserService } from '../../modules/user/service/user.service';
-
-export type Key = {
-  secret: string;
-  expiresIn: string;
-};
+import { TokenService } from './token.service';
 
 @Injectable()
 export class AuthService {
-  readonly ACCESS_SECRET_KEY: string = 'accessSecretKey';
-  readonly REFRESH_SECRET_KEY: string = 'refreshSecretKey';
-  readonly ACCESS_KEY_EXPIRATION_TIME: string = '15m';
-  readonly REFRESH_KEY_EXPIRATION_TIME: string = '7d';
-
   constructor(
-    private readonly jwtService: JwtService,
     private readonly userService: UserService,
-    private readonly refreshTokenRepository: RefreshTokenRepository,
+    private readonly tokenService: TokenService,
   ) {}
-
-  async generateAccessToken(payload: any, accessKey: Key): Promise<string> {
-    return this.jwtService.sign(payload, {
-      secret: accessKey.secret,
-      expiresIn: accessKey.expiresIn,
-    });
-  }
-
-  async generateRefreshToken(user: User, refreshKey: Key): Promise<string> {
-    const refreshToken = this.jwtService.sign(
-      { username: user.username },
-      {
-        secret: refreshKey.secret,
-        expiresIn: refreshKey.expiresIn,
-      },
-    );
-
-    const sevenDaysFromNow: number = Date.now() + 7 * 24 * 60 * 60 * 1000;
-    const expirationDate: Date = new Date(sevenDaysFromNow);
-
-    await this.refreshTokenRepository.save(
-      this.getRefreshToken(refreshToken, user, expirationDate),
-    );
-
-    return refreshToken;
-  }
-
-  async verifyAccessTokenOrThrow(
-    accessToken: string,
-    accessKey: Key,
-  ): Promise<any> {
-    try {
-      return this.jwtService.verify(accessToken, {
-        secret: accessKey.secret,
-      });
-    } catch (e) {
-      throw new Error('Access token verification failed');
-    }
-  }
-
-  async verifyRefreshTokenOrThrow(
-    refreshToken: string,
-    refreshKey: Key,
-  ): Promise<any> {
-    const existingRefreshToken =
-      await this.refreshTokenRepository.findOneByToken(refreshToken);
-    if (!existingRefreshToken) {
-      throw new Error('Invalid refresh token');
-    }
-    try {
-      return this.jwtService.verify(refreshToken, {
-        secret: refreshKey.secret,
-      });
-    } catch (e) {
-      throw new Error('Refresh token verification failed');
-    }
-  }
-
-  async refreshAccessTokenOrThrow(
-    refreshToken: string,
-    refreshKey: Key,
-    accessKey: Key,
-  ): Promise<string> {
-    const payload = await this.verifyRefreshTokenOrThrow(
-      refreshToken,
-      refreshKey,
-    );
-    const user = await this.userService.findByUsernameOrThrow(payload.username);
-    return this.generateAccessToken({ username: user.username }, accessKey);
-  }
-
-  async revokeRefreshToken(token: string): Promise<void> {
-    await this.refreshTokenRepository.deleteByToken(token);
-  }
-
-  getRefreshToken(
-    refreshToken: string,
-    user: User,
-    expirationDate: Date,
-  ): RefreshToken {
-    return {
-      token: refreshToken,
-      user,
-      expiresIn: expirationDate,
-    } as RefreshToken;
-  }
 }
