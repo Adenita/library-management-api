3ac782e31cfd3423055490c130199ea4df95e816
adenita <nittaluta@gmail.com>
2024-07-12 00:56:10 +0200

    add book mapper

---
+++ src/modules/book/mapper/book.mapper.ts
@@ -0,0 +1,18 @@
+import { Book } from '../entity/book.entity';
+import { GeneralMapper } from '../../../shared/general.mapper';
+import { BookShortDto } from '../dto/book-short.dto';
+import { BookListDto } from '../dto/book-list.dto';
+
+export class BookMapper {
+  static toListTransport(books: Book[]): BookListDto {
+    return { items: this.toListDto(books) } as BookListDto;
+  }
+
+  private static toListDto(books: Book[]): BookShortDto[] {
+    return books.map((book: Book) => GeneralMapper.toDto(BookShortDto, book));
+  }
+
+  static toEntity(dto: any): Book {
+    return GeneralMapper.toEntity(Book, dto);
+  }
+}
