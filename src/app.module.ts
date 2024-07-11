c820a1fea6e1a233602c142571cc0012a2fac45b
adenita <nittaluta@gmail.com>
2024-07-12 00:50:32 +0200

    add book module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './modules/user/user.module';
+import { BookModule } from './modules/book/book.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { UserModule } from './modules/user/user.module';
     DatabaseModule,
     AuthModule,
     UserModule,
+    BookModule,
   ],
   controllers: [AppController],
   providers: [AppService],
