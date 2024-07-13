5b45f41683f9289a4726ef69384075fe535edb90
adenita <nittaluta@gmail.com>
2024-07-13 23:08:54 +0200

    add expose annotation

--- src/modules/book/dto/book-short.dto.ts
+++ src/modules/book/dto/book-short.dto.ts
@@ -1,9 +1,20 @@
 import { CategoryType } from '../entity/category.enum';
 import { AuthorShortDto } from '../../author/dto/author-short.dto';
+import { Expose } from 'class-transformer';
 
 export class BookShortDto {
+  @Expose()
+  id: string;
+
+  @Expose()
   title: string;
+
+  @Expose()
   language: string;
+
+  @Expose()
   category: CategoryType;
+
+  @Expose()
   author: AuthorShortDto;
 }
