654ca4f9e3bec93270dffc6f954d829f4047912c
adenita <nittaluta@gmail.com>
2024-07-14 23:16:11 +0200

    add loan controller

--- src/modules/loan/loan.module.ts
+++ src/modules/loan/loan.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoanRepository } from './repository/loan.repository';
 import { Loan } from './entity/loan.entity';
 import { LoanService } from './service/loan.service';
+import { LoanController } from './controller/loan.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Loan, LoanRepository])],
-  controllers: [],
+  controllers: [LoanController],
   providers: [LoanService, LoanRepository],
 })
 export class LoanModule {}
