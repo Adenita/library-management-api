6b174c8e337265955b2f5a995042c9834c386e45
adenita <nittaluta@gmail.com>
2024-07-17 20:00:53 +0200

    add refresh token dto

---
+++ src/auth/dto/refresh-token.dto.ts
@@ -0,0 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class RefreshTokenDto {
+  @ApiProperty({
+    description: 'The refresh token used to obtain a new access token',
+  })
+  @IsString()
+  @IsNotEmpty()
+  token: string;
+}
