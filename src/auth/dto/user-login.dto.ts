52eb0d4034b85269b1495e01b8eb1180dad2cf41
adenita <nittaluta@gmail.com>
2024-07-17 20:01:40 +0200

    add swagger api property to auth dtos

--- src/auth/dto/user-login.dto.ts
+++ src/auth/dto/user-login.dto.ts
@@ -1,10 +1,19 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UserLoginDto {
+  @ApiProperty({
+    example: 'john_doe',
+    description: 'The username of the user',
+  })
   @IsString()
   @IsNotEmpty()
   username: string;
 
+  @ApiProperty({
+    example: 'password123',
+    description: 'The password of the user',
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
