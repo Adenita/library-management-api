df3b4e9993b12085630ce046e6f67276a0f7f648
adenita <nittaluta@gmail.com>
2024-07-17 20:34:39 +0200

    add swagger documentation to author dtos

--- src/modules/author/dto/author-create.dto.ts
+++ src/modules/author/dto/author-create.dto.ts
@@ -1,6 +1,11 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AuthorCreateDto {
+  @ApiProperty({
+    example: 'Jane Austen',
+    description: 'The full name of the author',
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
--- src/modules/author/dto/author-short.dto.ts
+++ src/modules/author/dto/author-short.dto.ts
@@ -1,9 +1,17 @@
 import { Expose } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AuthorShortDto {
+  @ApiProperty({
+    description: 'The author id',
+  })
   @Expose()
   id: string;
 
+  @ApiProperty({
+    example: 'Jane Austen',
+    description: 'The full name of the author',
+  })
   @Expose()
   name: string;
 }
