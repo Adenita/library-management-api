3f93900dc21e1e7e59f6fe7ec3e6c1037515aeed
adenita <nittaluta@gmail.com>
2024-07-17 20:43:48 +0200

    add swagger documentation to book dtos

--- src/modules/book/dto/book-create.dto.ts
+++ src/modules/book/dto/book-create.dto.ts
@@ -8,24 +8,45 @@ import {
 import { CategoryType } from '../entity/category.enum';
 import { AuthorShortDto } from '../../author/dto/author-short.dto';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class BookCreateDto {
+  @ApiProperty({
+    example: 'Little Women',
+    description: 'The title of the book',
+  })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({
+    example: 'English',
+    description: 'The language of the book',
+  })
   @IsString()
   @IsNotEmpty()
   language: string;
 
+  @ApiProperty({
+    example: 'ROMANCE',
+    description: 'The category of the book',
+  })
   @IsEnum(CategoryType)
   @IsNotEmpty()
   category: CategoryType;
 
+  @ApiProperty({
+    example: '5',
+    description: 'The number of copies left in the register',
+  })
   @IsNumber()
   @IsNotEmpty()
   availableCopies: number;
 
+  @ApiProperty({
+    example: '[author1, author2]',
+    description: 'The books author/authors',
+  })
   @ValidateNested()
   @Type(() => AuthorShortDto)
   authors: AuthorShortDto[];
