70d1144c532d366c9186193eca61d666cac3e580
adenita <nittaluta@gmail.com>
2024-07-17 18:25:44 +0200

    initialise swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  const config = new DocumentBuilder()
+    .setTitle('Library Management API')
+    .setDescription(
+      'This API allows for the management of library resources, including books, authors, and user interactions. It provides endpoints for creating, updating, retrieving, and deleting library-related data.',
+    )
+    .setVersion('1.0')
+    .addTag('lib')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
